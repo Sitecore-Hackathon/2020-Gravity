5430b62d3e2d34cde840dfe812a077dc5549f71d
Mohammed Syam <Mohammed.Syam@americaneagle.com>
2020-02-29 00:20:02 -0600

    Adding Teams feature

---
+++ src/Feature/Teams/code/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Sitecore.Data;
+
+namespace Hackathon.Feature.Teams
+{
+    public class Constants
+    {
+        internal class Settings
+        {
+            public static readonly ID TeamProfileId = new ID("{FB006FB6-4B0D-4EE3-810E-43EDC720746A}");
+        }
+
+        internal class DBNames
+        {
+            public const string Master = "master";
+        }
+    }
+}
---
+++ src/Feature/Teams/code/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackathon.Feature.Teams.Models
+{
+    public class Team
+    {
+        public string TeamName { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
