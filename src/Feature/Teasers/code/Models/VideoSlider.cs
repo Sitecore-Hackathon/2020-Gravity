ff7753cff599174f398e4878c0998260a66f61f6
Mohammed Syam <Mohammed.Syam@americaneagle.com>
2020-02-29 05:25:34 -0600

    Initial commit Teasers feature

---
+++ src/Feature/Teasers/code/Models/VideoSlider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackathon.Feature.Teasers.Models
+{
+    public class VideoSlider : ISliderItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string VideoUrl { get; set; }
+        public string LinkUrl { get; set; }
+        public string LinkTarget { get; set; }
+    }
+}
