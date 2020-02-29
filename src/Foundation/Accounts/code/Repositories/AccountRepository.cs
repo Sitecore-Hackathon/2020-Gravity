d621801fcbceecd828db7ed0d229d1d1811c91d4
Hamzeh Aledous <hamzeh.aledous@americaneagle.com>
2020-02-29 00:22:34 -0600

    Adding Foundations projects

---
+++ src/Foundation/Accounts/code/Repositories/AccountRepository.cs
@@ -0,0 +1,87 @@
+﻿namespace Sitecore.Foundation.Accounts.Repositories
+{
+    using System;
+    using System.Web.Security;
+    using Sitecore.Diagnostics;
+    using Sitecore.Foundation.Accounts.Services;
+    using Sitecore.Pipelines;
+    using Sitecore.Security.Accounts;
+    using Sitecore.Security.Authentication;
+
+ 
+    public class AccountRepository : IAccountRepository
+    {
+
+        public bool Exists(string userName)
+        {
+            var fullName = Context.Domain.GetFullName(userName);
+
+            return User.Exists(fullName);
+        }
+
+        public User Login(string userName, string password)
+        {
+            var accountName = string.Empty;
+            var domain = Context.Domain;
+            if (domain != null)
+            {
+                accountName = domain.GetFullName(userName);
+            }
+
+            var result = AuthenticationManager.Login(accountName, password);
+            if (!result)
+            {
+                return null;
+            }
+
+            var user = AuthenticationManager.GetActiveUser();
+          
+            return user;
+        }
+
+        public void Logout()
+        {
+            var user = AuthenticationManager.GetActiveUser();
+            AuthenticationManager.Logout();
+           
+        }
+
+        public string RestorePassword(string userName)
+        {
+            var domainName = Context.Domain.GetFullName(userName);
+            var user = Membership.GetUser(domainName);
+            if (user == null)
+                throw new ArgumentException($"Could not reset password for user '{userName}'", nameof(userName));
+            return user.ResetPassword();
+        }
+
+        public void RegisterUser(string email, string password, string profileId)
+        {
+            Assert.ArgumentNotNullOrEmpty(email, nameof(email));
+            Assert.ArgumentNotNullOrEmpty(password, nameof(password));
+
+            var fullName = Context.Domain.GetFullName(email);
+            try
+            {
+
+                Assert.IsNotNullOrEmpty(fullName, "Can't retrieve full userName");
+
+                var user = User.Create(fullName, password);
+                user.Profile.Email = email;
+                if (!string.IsNullOrEmpty(profileId))
+                {
+                    user.Profile.ProfileItemId = profileId;
+                }
+
+                user.Profile.Save();
+             
+            }
+            catch
+            {
+                throw;
+            }
+
+            this.Login(email, password);
+        }
+    }
+}
