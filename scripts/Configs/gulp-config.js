f1d5d74119b511a67f92b72cd45356f8895c44f7
Developer Name <Developer Email Address>
2020-02-28 20:58:56 -0600

    Adding initial foundation modules

---
+++ scripts/Configs/gulp-config.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+    var instanceRoot = "C:\\inetpub\\wwwroot\\sc93.local.test";
+    var config = {
+        websiteRoot: instanceRoot + "\\",
+        sitecoreLibraries: instanceRoot + "\\bin",
+        licensePath: instanceRoot + "\\App_Data\\license.xml",
+        packageXmlBasePath: ".\\src\\Project\\Gravity.Project.Hackathon\\code\\App_Data\\packages\\Hackathon.xml",
+        packagePath: instanceRoot + "\\App_Data\\packages",
+        solutionName: "Hackathon.Boilerplate",
+        buildConfiguration: "Debug",
+        buildToolsVersion: '16.0',
+        buildMaxCpuCount: 0,
+        buildVerbosity: "minimal",
+        buildPlatform: "Any CPU",
+        publishPlatform: "AnyCpu",
+        runCleanBuilds: false
+    };
+    return config;
+}
